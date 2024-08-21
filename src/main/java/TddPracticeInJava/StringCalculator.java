0ece8e794989fa4194a752311d8d52168635f794
vinaykumarsanbal <vinayks803@gmail.com>
2024-08-21 18:16:52 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-	public int addition(String input) {
+	public int add(String input) {
 		if(input.isEmpty()) {
 			return 0;
 		}
